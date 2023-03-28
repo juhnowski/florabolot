3852f22de7aba0c3c3f2f37be6ba02a5707fe16b
Juhnowski <juhnowski@gmail.com>
2023-03-28 20:59:09 +0000

    docker image

--- flora/src/main/java/flora/LoadDatabase.java
+++ flora/src/main/java/flora/LoadDatabase.java
@@ -1,5 +1,9 @@
 package flora;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Arrays;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -12,10 +16,31 @@ class LoadDatabase {
 
 	@Bean
 	CommandLineRunner initDatabase(FloraRepository repository) {
+		return args -> {log.info("Skiped");} ;
+		// return args -> {
+        //     try (FileReader fr = new FileReader("/home/ilya/florabolot/data.csv");
+        //          BufferedReader br = new BufferedReader(fr);
+        //          ) {
+        //         String line ="";
 
-		return args -> {
-			log.info("Preloading " + repository.save(new Flora("Athyrium filix-femina (L.) Roth", "Кочедыжник женский "," "," "," "," ","1"," "," "," ")));
-			log.info("Preloading " + repository.save(new Flora("Athyrium filix-femina (L.) Roth", "Кочедыжник женский "," "," "," "," ","1"," "," "," ")));
-		};
+        //         while (line!=null) {
+        //             line = br.readLine();
+        //             if (line!=null){
+        //                 String[] values=line.split(";");
+		// 				//log.info("Preloading " + repository.save(new Flora("Athyrium filix-femina (L.) Roth", "Кочедыжник женский "," "," "," "," ","1"," "," "," ")));
+		// 				log.info("Preloading " + repository.save(new Flora(values[0],
+		// 				 values[1],
+		// 				 values[2],
+		// 				 values[3],
+		// 				 values[4],
+		// 				 values[5],
+		// 				 values[6],
+		// 				 values[7],
+		// 				 values[8],
+		// 				 values[9])));
+		// 			}
+		// 		}
+		// 	}
+		// };
 	}    
 }
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 mvn clean spring-boot:run
 curl -v localhost:8080/flora
 
-DOCKER_BUILDKIT=1 docker build -t juhnowski/florabolot .
+sudo DOCKER_BUILDKIT=1 docker build -t juhnowski/florabolot .
